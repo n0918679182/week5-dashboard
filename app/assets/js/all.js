84e24137e1124e0b5c599729689c556ad259e8c2
waynechen <n0918679182@gmail.com>
2022-08-07 16:55:03 +0800

    新增文字編輯區

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,14 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+$(document).ready(function () {
+  $('#reply').click(function (e) { 
+    e.preventDefault();
+    $('#all-response').addClass('d-none');
+    $('#response-content').removeClass('d-none');
+    $('#response-ctrl').addClass('d-none');
+  });
+  $('#reply-send').click(function (e) { 
+    e.preventDefault();
+    $('#all-response').removeClass('d-none');
+    $('#response-content').addClass('d-none');
+    $('#response-ctrl').removeClass('d-none');
+  });
 });
